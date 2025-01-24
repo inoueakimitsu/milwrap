417bb4a961835a04b85a37897904e073b955f307
Akimitsu Inoue <akimitsu.inoue@gmail.com>
2025-01-24 05:21:38 +0000

    refactor(version): use dynamic version from poetry/commitizen

--- milwrap/__version__.py
+++ milwrap/__version__.py
@@ -1,14 +1,15 @@
-"""
-__version__.py
-~~~~~~~~~~~~~~
+"""Information about the current version of the milwrap package."""
 
-Information about the current version of the miwrawp package.
-"""
+from importlib.metadata import PackageNotFoundError, version
 
-__title__ = 'milwrap'
-__description__ = 'milwrap - multiple instane meta-learner that can use any supervised-learning algorithms.'
-__version__ = '0.1.3'
-__author__ = 'Akimitsu Inoue'
-__author_email__ = 'akimitsu.inoue@gmail.com'
-__license__ = 'MIT'
-__url__ = 'https://github.com/inoueakimitsu/milwrap'
+__title__ = "milwrap"
+__description__ = "milwrap - multiple instane meta-learner that can use any supervised-learning algorithms."
+try:
+    __version__ = version(__title__)
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = "unknown"
+__author__ = "Akimitsu Inoue"
+__author_email__ = "akimitsu.inoue@gmail.com"
+__license__ = "MIT"
+__url__ = "https://github.com/inoueakimitsu/milwrap"
